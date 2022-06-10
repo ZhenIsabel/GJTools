2593c5718c29da9441135597f221f7bcd6be3955
zhenIsabel <liangtianning@outlook.com>
2022-06-11 03:43:36 +0800

    update: 邮箱进配置

--- send_message.py
+++ send_message.py
@@ -8,16 +8,16 @@ import traceback
 def send_message(message):
      try:
         # 填写真实的发邮件服务器用户名、密码
-        receive_addr = config.config['email_add']
-        user = 'code_test_message@163.com'
-        password = 'RYXVVTELHMNPHJUY'
+        receive_addr = config.config['receive_email_add']
+        user = config.config['send_email_add']
+        password = config.config['send_email_pw']
         # 邮件内容
         msg = MIMEText(message, 'plain', _charset="utf-8")
         msg["Subject"] = "GJTool"
         msg["from"] = user
         msg["to"] = receive_addr
         msg["Cc"] = ""
-        with SMTP_SSL(host="smtp.163.com") as smtp:
+        with SMTP_SSL(host=config.config['send_email_host']) as smtp:
             # 登录发邮件服务器
             smtp.login(user=user, password=password)
             # 实际发送、接收邮件配置
