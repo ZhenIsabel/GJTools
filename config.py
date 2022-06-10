0516e1a2951c23b1a785a0d39d6ab498af0d5f63
zhenIsabel <liangtianning@outlook.com>
2022-06-11 03:34:29 +0800

    fix: 当识别不到牌时顺序出牌纠错

--- config.py
+++ config.py
@@ -4,5 +4,7 @@ config = {
     'email_add': 'code_test_message@163.com',  # 接收报错邮件的地址
     'key_commu': 'g',  # 交互键
     'longxing_pos':[1280,600],# 屏幕中心位置
-    'screen':[2560,1440] # 屏幕大小
+    'screen':[2560,1440], # 屏幕大小
+    'first_card_loc_in_pool':[677,645],# 卡池中第一张卡的位置
+    'card_space_in_pool':85,# 卡池中卡牌间隔像素
 }
--- test.py
+++ test.py
@@ -7,3 +7,6 @@ import pyautogui
 import config
 
 time.sleep(0.5)
+template=cv2.imread('img/blue.png')
+image = cv2.cvtColor(np.asarray(pyautogui.screenshot(region=config.config['card_pool_region'])), cv2.COLOR_RGB2BGR)
+utils.show_match_image(template,  image)
